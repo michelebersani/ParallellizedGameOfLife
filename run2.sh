593fd5122ba83fe240c46524c154e96946017f01
Michele Bersani <49282359+michelebersani@users.noreply.github.com>
2021-07-21 02:59:52 +0200

    cleaning bash script and other stuff

--- run2.sh
+++
@@ -1,8 +0,0 @@
-for side in 1000 10000; do
-   for nw in 1 2 3 4 5 6 7 8 ; do
-      echo $side $nw
-      for ((iter = 0; iter <= 5; iter++)) do
-        ./build/threadpool $side $side 5 $iter $nw >> ./results_mac/new_thrp_${side}_${nw}.txt
-      done
-   done
-done
